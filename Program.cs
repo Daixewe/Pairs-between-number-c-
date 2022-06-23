7ec5eede04489faa1d7bbe2e14014ee51691a662
Daixewe <fuentesvd56@gmail.com>
2022-06-22 21:07:18 -0600

    Pairs between numbers

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Evens numbers betwwn 2 numbers\n");
+
+Console.WriteLine("Enter the first number: ");
+int firtsNumber = Int32.Parse(Console.ReadLine() ?? "o");
+
+Console.WriteLine("Enter the second number: ");
+int secondNumber = Int32.Parse(Console.ReadLine() ?? "1000");
+
+for (int i = firtsNumber; i <= secondNumber; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+Console.WriteLine("End of the program");
